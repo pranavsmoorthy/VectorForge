86d3703d12d89bedfff45fd43ca996dac7d00927
pranavsmoorthy <pranavsmoorthy@gmail.com>
2026-06-12 21:31:40 -0400

    Added flat_index header file

---
+++ src/flat_index/flat_index.h
@@ -0,0 +1,75 @@
+#ifndef HNSW_SRC_FLAT_INDEX_H_
+#define HNSW_SRC_FLAT_INDEX_H_
+
+#include "src/vector_base/vector_base.h"
+
+#include <vector>
+
+namespace hierarchial_navigable_small_world {
+namespace flat_index {
+
+/**
+ * This is meant for testing. The class will be representing a linear search
+ * and will be used to compare against the final HNSW graph.
+ */
+class FlatIndex {
+    public:
+        /**
+         * Constructor:
+         * Single constructor that takes the size of the VectorBase objects,
+         * which will be enforced.
+         */
+
+        explicit FlatIndex(std::size_t vector_size);
+
+        /**
+         * Rule of 5:
+         * 1. Destructor - Frees the resource
+         * 2. Copy Constructor - Creates deep copy of object
+         * 3; Copy Assignment - Overwrites existing via deep copy
+         * 4. Move Constructor - Transfer ownership of resources from temporary
+         *    object to an new one without copying
+         * 5. Move Assignment - Transfer ownership of resources from a 
+         *    temporary object to an existing one
+         */
+
+        ~FlatIndex();
+        FlatIndex(const FlatIndex& other);
+        FlatIndex& operator=(const FlatIndex& other);
+        FlatIndex(FlatIndex&& other) noexcept;
+        FlatIndex& operator=(FlatIndex&& other) noexcept;
+
+        /**
+         * Getters and Setters:
+         * One getter, which returns the data of the FlatIndex. AddData adds a 
+         * pointer to the underlying data vector.
+         */
+
+        const std::vector<vector_base::VectorBase*>& GetData() const;
+        void AddData(const vector_base::VectorBase& vector_pointer);
+
+        /**
+         * Search:
+         * Performs a linear search on the data. This is mainly for testing and
+         * acts as a benchmark for the HNSW graph to compare against.
+         */
+
+        std::vector<vector_base::VectorBase> Search(
+            const vector_base::VectorBase& query, std::size_t k) const;
+    
+    private:
+        /**
+         * The size the vectors must be in order to be added to the data vector
+         */
+        std::size_t size_constraint_;
+
+        /**
+         * The data vector
+         */
+        std::vector<vector_base::VectorBase*> data_;
+};
+
+};
+};
+
+#endif
--- src/vector_base/vector_base.h
+++ src/vector_base/vector_base.h
@@ -5,64 +5,69 @@
 
 namespace hierarchial_navigable_small_world {
 namespace vector_base {
-    class VectorBase {
-        public:
-            /**
-             * Constructors:
-             * One default no arg constructor and one explicit constructor that 
-             * takes a double vector to initialize 
-             */
 
-            VectorBase();
-            explicit VectorBase(const std::vector<double>& initial_coords);
+/**
+ * Acts as a representation of a high dimensional vector.
+ */
+class VectorBase {
+    public:
+        /**
+         * Constructors:
+         * One default no arg constructor and one explicit constructor that 
+         * takes a double vector to initialize 
+         */
 
-            /**
-             * Getters and Setters:
-             * Get and replace coords in current VectorBase object with new 
-             * coordinates. Get number of coordinates in a VectorBase object,
-             * read only
-             */
+        VectorBase();
+        explicit VectorBase(const std::vector<double>& initial_coords);
 
-            void SetCoords(const std::vector<double>& coords);
-            const std::vector<double>& GetCoords() const;
-            std::size_t GetNumCoords() const;
+        /**
+         * Getters and Setters:
+         * Get and replace coords in current VectorBase object with new 
+         * coordinates. Get number of coordinates in a VectorBase object,
+         * read only
+         */
 
-            /**
-             * Arithmetic Operator Overload:
-             * Overload the operators for + and - for easier addition and
-             * subtraction of vectors. Other VectorBase is left constant
-             */
+        void SetCoords(const std::vector<double>& coords);
+        const std::vector<double>& GetCoords() const;
+        std::size_t GetNumCoords() const;
 
-            VectorBase operator+(const VectorBase& other) const;
-            VectorBase operator-(const VectorBase& other) const;
+        /**
+         * Arithmetic Operator Overload:
+         * Overload the operators for + and - for easier addition and
+         * subtraction of vectors. Other VectorBase is left constant
+         */
 
-            /**
-             * Array Operators Overload:
-             * Overload the square bracket [] operators with read only and 
-             * write options that take a size_t index;
-             */
+        VectorBase operator+(const VectorBase& other) const;
+        VectorBase operator-(const VectorBase& other) const;
 
-            double& operator[](std::size_t index);
-            double operator[](std::size_t index) const;
+        /**
+         * Array Operators Overload:
+         * Overload the square bracket [] operators with read only and 
+         * write options that take a size_t index;
+         */
 
-            /**
-             * Other operations:
-             * Includes finding the length of the vector and euclidean 
-             * distance to another vector, both of which use pythagorean 
-             * theorem, as well as the cosine similarity. All are read only.
-             */
+        double& operator[](std::size_t index);
+        double operator[](std::size_t index) const;
 
-            double Length() const;
-            double EuclideanDistanceTo(const VectorBase& other) const;
-            double CosineSimilarityTo(const VectorBase& other) const;
+        /**
+         * Other operations:
+         * Includes finding the length of the vector and euclidean 
+         * distance to another vector, both of which use pythagorean 
+         * theorem, as well as the cosine similarity. All are read only.
+         */
 
-        private:
-            /**
-             * Holds that actual data of the VectorBase
-             */
-            
-            std::vector<double> coords_;
-    };
+        double Length() const;
+        double EuclideanDistanceTo(const VectorBase& other) const;
+        double CosineSimilarityTo(const VectorBase& other) const;
+
+    private:
+        /**
+         * Holds that actual data of the VectorBase
+         */
+        
+        std::vector<double> coords_;
+};
+
 }
 }
 
